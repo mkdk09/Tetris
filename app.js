1a603a2f50a77c06dd8b31d2e93b813006c0972d
mkdk09 <mkdk099@gmail.com>
2020-04-12 00:17:43 +0900

    update js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const mapWeight = 300;
+const mapWidth = 300;
 const mapHeight = 600;
 const numberOfRows = 20;
 const numberOfCols = 10;
-const cellSize = mapWeight / numberOfCols;
+const cellSize = mapWidth / numberOfCols;
 const borderSize = 0.2;
 
 const zType = [
@@ -74,6 +74,40 @@ class Block {
         this.isAlive = true;
     }
 
+    rotate() {
+        const newCells = [];
+        for (let i = 0; i < this.cells.length; i++) {
+            newCells[i] = [];
+            for (let j = 0; j < this.cells.length; j++) {
+                newCells[i][j] = this.cells[this.cells.length - 1 - j][i];
+            }
+        }
+        this.cells = newCells;
+    }
+
+    moveBlockByEvent(e) {
+        switch(e.key) {
+            case 'ArrowLeft': {
+                this.position.x--;
+                break;
+            }
+            case 'ArrowRight': {
+                this.position.x++;
+                break;
+            }
+            case 'ArrowDown': {
+                if (this.position.y + this.cells.length < numberOfRows) {
+                    this.position.y++;
+                }
+                break;
+            }
+            case 'ArrowUp': {
+                this.rotate()
+                break;
+            }
+        }
+    }
+
     findCollison(field) {
         const { x, y } = this.position
         this.cells.forEach((rows, i) => {
@@ -87,8 +121,45 @@ class Block {
     }
 }
 
-const checkCanMove = () => {
+Block.timeToChange = 1000;
+
+const checkCanMove = (block, field) => {
+    const { cells, position } = block;
+    const { x, y } = position;
+    return !cells.some((rows, i) => {
+        return rows.some((cell, j) => {
+            if (
+                (cell && x + j < 0) ||
+                (cell && x + j >= numberOfCols) ||
+                (cell && field[y + i][x + j])
+            ) return true;
+        })
+    })
     return true;
+}
+
+const updateScore = (score) => {
+    const scoreElem = document.getElementById('score');
+    scoreElem.innerHTML = score;
+}
+
+const drawField = (field, ctx) => {
+    console.log(field);
+    field.forEach((row, rowIndex) => {
+        row.forEach((cell, columnIndex) => {
+            ctx.fillStyle = cell ? blockColors[cell - 1] : 'lightblue';
+            ctx.strokeStyle = '#555';
+            ctx.lineWidth = borderSize;
+
+            const args = [
+                columnIndex * cellSize, rowIndex * cellSize,
+                cellSize, cellSize,
+            ]
+
+            ctx.fillRect(...args);
+            ctx.strokeRect(...args);
+        })
+    })
 }
 
 const { requestAnimationFrame } = window;
@@ -105,8 +176,9 @@ const render = (game, block, time) => {
         const arrOfTypes = Object.values(blockTypes);
         const blockType = arrOfTypes[arrOfTypes.length * Math.random() | 0];
         const x = ((numberOfCols - blockType.length) / 2) | 0;
-        block = new Block(blockType, x, 0)
+        block = new Block(blockType, x, 0);
         prevPosition = { x, y: 0 };
+        addEventListener('keydown', (e) => block.moveBlockByEvent.bind(block)(e));
     }
 
     const { ctx, field } = game;
@@ -122,25 +194,43 @@ const render = (game, block, time) => {
                 block = null;
             }
         }
+        prevTime = time;
+    
+        insertIntoArray(prevBlockCells, field, prevPosition.y, prevPosition.x, true);
+    
+        const canMove = checkCanMove(block, field);
+        if (!canMove) {
+            position.x = prevPosition.x;
+            block.cells = prevBlockCells;
+        }
+    
+        if (position.y > prevPosition.y) {
+            position.y = prevPosition.y + 1;
+        }
+    
+        block.findCollison(field);
+        if (block.isAlive) {
+            insertIntoArray(block.cells, field, position.y, position.x);
+            drawField(field, ctx);
+            prevPosition = Object.assign({}, position);
+            prevBlockCells = [].concat(block.cells);
+        } else if (prevPosition.y > block.cells.length - 1) {
+            insertIntoArray(block.cells, field, prevPosition.y, prevPosition.x);
+            game.field = findFilledRow(field);
+            drawField(game.field, ctx);
+            block = null;
+        } else {
+            insertIntoArray(block.cells, field, prevPosition.y, prevPosition.x);
+            const lastBlock = block.cells.filter((row) => !row.every((cell) => !cell)).slice(-prevPosition.y);
+            insertIntoArray(lastBlock, field, 0, position.x);
+            drawField(game.field, ctx);
+            setTimeout(() => { alert('Game Over') }, 0);
+            game.field = generateField(numberOfRows + 4, numberOfCols);
+            updateScore(0);
+            block = null;
+        }
     }
 
-    prevTime = time;
-
-    insertIntoArray(prevBlockCells, field, prevPosition.y, prevPosition.x, true);
-
-    const canMove = checkCanMove(block, field);
-    if (!canMove) {
-        position.x = prevPosition.x;
-        block.cells = prevBlockCells;
-    }
-
-    block.findCollison(field);
-    if (block.isAlive) {
-        insertIntoArray(block.cells, field, position.y, position.x);
-        drawField(field, ctx);
-        prevPosition = Object.assign({}, position);
-        prevBlockCells = [].concat(block.cells);
-    }
 
     requestAnimationFrame((time) => render(game, block, time));
 
@@ -164,9 +254,20 @@ const insertIntoArray = (childArr, parrentArr, row, col, clearMode) => {
     }
 }
 
+let score = 0;
+const findFilledRow = (field) => {
+    const filteredField = field.filter((row) => row.some((cell) => (cell === 0)));
+    const diff = field.length - filteredField.length;
+    score += diff * 100;
+    updateScore(score);
+    const filledArr = generateField(diff, numberOfCols);
+    return [...filledArr, ...filteredField];
+}
+
 const generateField = (rows, cols) => {
     const field = Array.from({length: rows},
-        () => Array.from({length: cols}, () => 0))
+        () => Array.from({length: cols}, () => 0));
+    return field;
 }
 
 window.onload = () => {
